15bbaffb3ac67e04b48a58bf44dcfd3659ed37dc
dipansh17 <dipanshmalhotra11@gmail.com>
2024-12-02 15:03:40 +0530

    Fixed and completed the backend

---
+++ backend-1/src/Game.ts
@@ -0,0 +1,68 @@
+import { WebSocket } from "ws";
+import {BLACK, Chess} from "chess.js"
+import { GAME_OVER,INIT_GAME,MOVE } from "./messages";
+export class Game{
+    public player1:WebSocket;
+    public player2:WebSocket;
+    private board: Chess;
+    private startTime:Date;
+    private moveCount:number;
+
+    constructor(player1:WebSocket,player2:WebSocket){
+        this.player1=player1;
+        this.player2=player2;
+        this.board=new Chess();
+        this.startTime=new Date();
+        this.moveCount=0;
+
+        this.player1.send(JSON.stringify({
+            type:INIT_GAME,
+            payload:{
+                color:"white"
+            }
+        }))
+        this.player2.send(JSON.stringify({
+            type:INIT_GAME,
+            payload:{
+                color:"black"
+            }
+        }))
+    }
+    makeMove(socket:WebSocket,move:{
+        //validation if this user move and if move valid
+        from :string;
+        to:string;
+    }){
+        if(this.moveCount%2==0 && socket!==this.player1)return;
+        if(this.moveCount%2==1 && socket!==this.player2)return;
+        try {
+            this.board.move(move);
+              
+        } catch (error) {
+            console.log(error);
+            return ;
+        }
+        if(this.board.isGameOver()){
+            this.player1.send(JSON.stringify({
+                type:GAME_OVER,
+                payload:{
+                    winner:this.board.turn()==='w'? "black":"white"
+                }
+            }))
+            return;
+        }
+        if(this.board.moves().length%2==0){
+            this.player2.send(JSON.stringify({
+                type:MOVE,
+                payload:move
+            }))
+        }else{
+            this.player1.send(JSON.stringify({
+                type:MOVE,
+                payload:move
+            }))
+        }
+        
+        this.moveCount++;   
+    }
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <button className='bg-red-500'>Join Room</button>
+       </div>
+    </>
+  )
+}
+
+export default App
